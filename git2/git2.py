6fdd0a08f4335b17060074d957fed7f3e16ee723
Facundo P <facupiscitello01@gmail.com>
2020-10-26 22:42:54 -0300

    Agregada funcion

--- git2/git2.py
+++ git2/git2.py
@@ -45,3 +45,14 @@ def elem_comun(lista1, lista2) -> str:
                resultado.append(element)
        return resultado
     
+def palindroma (str) -> str:
+    igual, aux = 0, 0
+    palabra = []
+    for ind in reversed(range(0, len(palabra))):
+        if palabra[ind].lower() == palabra[aux].lower():
+         igual += 1
+         aux += 1
+        if len(palabra) == igual:
+          return "El texto es palindromo"
+        else:
+          print("El texto no es palindromo!")
